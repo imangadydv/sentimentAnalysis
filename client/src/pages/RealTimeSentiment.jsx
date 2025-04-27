492089f6d15976e7a858efd19abce267e38cafe0
imangadydv <148368906+imangadydv@users.noreply.github.com>
2025-04-28 01:21:57 +0530

    Update RealTimeSentiment.jsx

--- client/src/pages/RealTimeSentiment.jsx
+++ client/src/pages/RealTimeSentiment.jsx
@@ -15,7 +15,7 @@ const RealTimeSentiment = () => {
     if (!imageSrc) return;
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/start-detecting", {
+      const response = await fetch("https://sentimentanalysis-ml.onrender.com/start-detecting", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ image: imageSrc }),
